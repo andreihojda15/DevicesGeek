2563e90879d452dbfd00652738ba476699011af3
andreihojda15 <andrei.hojda00@gmail.com>
2022-11-23 11:22:08 +0200

    [BE] connected to db using mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connect = require("./config/database");
 const app = express();
 require("dotenv").config();
+
+connect().catch((err) => console.log(err));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send(`Hi from root!`);
